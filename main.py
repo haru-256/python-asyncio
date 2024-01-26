5defdc8b40467e20b5292cec076fcafc8c2eb18c
Yohei Okabayashi <yohei.kuro48@gmail.com>
2024-01-26 23:21:59 +0900

    await task

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import asyncio
 
 
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+
 async def main():
-    print("Hello ...")
+    task1 = asyncio.create_task(say_after(3, "hello"))
+    task2 = asyncio.create_task(say_after(3, "world"))
+    task3 = asyncio.create_task(say_after(3, "haru256"))
+    await task1
+    await task2
+    await task3
 
 
 if __name__ == "__main__":
-    asyncio.run(main)
+    asyncio.run(main())
