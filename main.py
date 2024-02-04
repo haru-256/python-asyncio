47936c8073c3b75492f7310d6769a24fe9e9d2b6
Yohei Okabayashi <yohei.kuro48@gmail.com>
2024-02-04 20:50:38 +0900

    ADD: add decolator to mesure exection time

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import asyncio
 
+from utils import async_timed
 
+
+@async_timed()
 async def say_after(delay, what):
     await asyncio.sleep(delay)
     print(what)
 
 
+@async_timed()
 async def main():
     task1 = asyncio.create_task(say_after(3, "hello"))
     task2 = asyncio.create_task(say_after(3, "world"))
---
+++ utils.py
@@ -0,0 +1,23 @@
+import functools
+import time
+from typing import Any, Callable
+
+
+def async_timed():
+    """非同期関数の実行時間を計測するデコレーター"""
+
+    def wrapper(func: Callable) -> Callable:
+        @functools.wraps(func)
+        async def wrapped(*args, **kwargs) -> Any:
+            print(f"starting {func} with args {args} {kwargs}")
+            start = time.time()
+            try:
+                return await func(*args, **kwargs)
+            finally:
+                end = time.time()
+                total = end - start
+                print(f"finished {func} in {total:.4f} second(s)")
+
+        return wrapped
+
+    return wrapper
